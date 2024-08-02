7de8a7977d47b3c089e8f922016009e6ffa4901c
EsmeMP <esmeralda.padilla.com@gmail.com>
2024-08-02 14:31:09 -0600

    Nueva actualizacion

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import psycopg2
-from flask import Flask, request, redirect, render_template, jsonify, url_for
+from flask import Flask, request, redirect, render_template, jsonify, url_for, session, flash
 # from flask_bootstrap import Bootstrap
 # from flask_wtf import FlaskForm
 # from wtforms.fields import PasswordField, StringField, SubmitField
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import db
+# from flask_bcrypt import Bcrypt
 # from forms import LibrosForm
 import bcrypt
 import os
@@ -17,25 +19,59 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'static/assets/img'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+    
 @app.route('/')
 # def hello():
 #     return "Hello World"
 
 # app.run (host = '0.0.0.0', port=3000)
 
-@app.route('/regVenta')
-def venta():
+
+
+
+
+@app.route('/seccion')
+def seccion():
+    return render_template('secciones.html')
+
+@app.route('/registrar_venta')
+def registrar_venta():
     conn =db.conectar()
     # crear un cursor (objeto para recorrer las tablas)
     cursor = conn.cursor()
     # ejecutar una consulta en postgres
-    cursor.execute('''SELECT * FROM venta''')
+    cursor.execute('''SELECT * FROM tabla_ventas''')
     #recuperar la informacion
     datos = cursor.fetchall()
     #cerrar cursos y conexion a la base de datos
     cursor.close()
     db.desconectar(conn)
     return render_template('regVenta.html', datos=datos)
+
+
+@app.route('/consultar_venta')
+def consultar_venta():
+    conn =db.conectar()
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conn.cursor()
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM tabla_ventas''')
+    #recuperar la informacion
+    datos = cursor.fetchall()
+    #cerrar cursos y conexion a la base de datos
+    cursor.close()
+    db.desconectar(conn)
+    return render_template('consultaVenta.html', datos=datos)
+
+
+
+
+
+
+
+
+
+
 
 # @app.route('/consultar_usuario')
 # def consultar_usuario():
@@ -82,79 +118,91 @@ def consultar_usuarios():
     return render_template('consultarUsuarios.html', datos=datos)
 
 
-@app.route('/registrar_usuario')
+# @app.route('/registrar_usuario')
+# def registrar_usuario():
+#     return render_template('regUsuario.html')
+
+
+
+@app.route('/registrar_usuario', methods=['GET', 'POST'])
 def registrar_usuario():
-    return render_template('regUsuario.html')
-
-
-
-@app.route('/registrar_usuario', methods=['POST'])
-def registrar_usuario_post():
-    nombre = request.form['nombre']
-    a_paterno = request.form['apellido_paterno']
-    a_materno = request.form['apellido_materno']
-    domicilio = request.form['domicilio']
-    numero_telefono = request.form['numero_telefonico']
-    curp = request.form['curp']
-    fecha_contratacion = request.form['date']
-    correo_electronico = request.form['correo_electronico']
-    username = request.form['nombre_usuario']
-    password = request.form['contraseña']
-    rol = request.form['tipos_de_usuario']
-
-    # Convertir rol a booleano
-    rol_booleano = True if rol == 'administrador' else False
-
-    file = request.files.get('imagen')
-    if file and file.filename != '':
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        ruta_imagen = os.path.join('assets', 'img', filename).replace('\\', '/')
-    else:
-        ruta_imagen = 'assets/img/imagen_defecto.jpeg'
-
-
-    conn = db.conectar()
-    cur = conn.cursor()
-    
-    try:
-        # Inicia una transacción
-        cur.execute("BEGIN;")
+    if request.method == 'POST':
+        # Comprobación de existencia de los campos
+        required_fields = [
+            'nombre', 'apellido_paterno', 'apellido_materno', 'domicilio', 
+            'numero_telefonico', 'curp', 'date', 'correo_electronico', 
+            'nombre_usuario', 'contraseña', 'tipos_de_usuario'
+        ]
         
-        # Inserta en info_empleado y obtiene el ID generado
-        cur.execute("""
-            INSERT INTO info_empleado (nombre, a_paterno, a_materno, domicilio, numero_telefono, curp, fecha_contratacion, correo_electronico, imagen)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-            RETURNING id_empleado;
-        """, (nombre, a_paterno, a_materno, domicilio, numero_telefono, curp, fecha_contratacion, correo_electronico, ruta_imagen))
+        for field in required_fields:
+            if field not in request.form:
+                return jsonify({"error": f"Missing field: {field}"}), 400
         
-        resultado = cur.fetchone()
-        if resultado:
-            nuevo_id_empleado = resultado[0]
+        nombre = request.form['nombre']
+        a_paterno = request.form['apellido_paterno']
+        a_materno = request.form['apellido_materno']
+        domicilio = request.form['domicilio']
+        numero_telefono = request.form['numero_telefonico']
+        curp = request.form['curp']
+        fecha_contratacion = request.form['date']
+        correo_electronico = request.form['correo_electronico']
+        username = request.form['nombre_usuario']
+        password = request.form['contraseña']
+        rol = request.form['tipos_de_usuario']
+
+        # Convertir rol a booleano
+        rol_booleano = True if rol == 'administrador' else False
+
+        file = request.files.get('imagen')
+        if file and file.filename != '':
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            ruta_imagen = os.path.join('assets', 'img', filename).replace('\\', '/')
         else:
-            raise Exception("No se pudo obtener el id_empleado generado.")
-        
-        # Inserta en usuario utilizando el ID generado
-        cur.execute("""
-            INSERT INTO usuario (fk_info_empleado, username, password, rol)
-            VALUES (%s, %s, %s, %s);
-        """, (nuevo_id_empleado, username, password, rol_booleano))
-        
-        # Finaliza la transacción
-        cur.execute("COMMIT;")
+            ruta_imagen = 'assets/img/imagen_defecto.jpeg'
 
+        conn = db.conectar()
+        cur = conn.cursor()
+        
+        try:
+            # Inicia una transacción
+            cur.execute("BEGIN;")
+            
+            # Inserta en info_empleado y obtiene el ID generado
+            cur.execute("""
+                INSERT INTO info_empleado (nombre, a_paterno, a_materno, domicilio, numero_telefono, curp, fecha_contratacion, correo_electronico, imagen)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                RETURNING id_empleado;
+            """, (nombre, a_paterno, a_materno, domicilio, numero_telefono, curp, fecha_contratacion, correo_electronico, ruta_imagen))
+            
+            resultado = cur.fetchone()
+            if resultado:
+                nuevo_id_empleado = resultado[0]
+            else:
+                raise Exception("No se pudo obtener el id_empleado generado.")
+            
+            # Inserta en usuario utilizando el ID generado
+            cur.execute("""
+                INSERT INTO usuario (fk_info_empleado, username, password, rol)
+                VALUES (%s, %s, %s, %s);
+            """, (nuevo_id_empleado, username, password, rol_booleano))
+            
+            # Finaliza la transacción
+            cur.execute("COMMIT;")
+
+            return render_template('regUsuario.html')
+
+        except Exception as e:
+            # En caso de error, deshace la transacción
+            conn.rollback()
+            return jsonify({"error": str(e)}), 400
+
+        finally:
+            # Cierra el cursor y la conexión
+            cur.close()
+            db.desconectar(conn)
+    else:
         return render_template('regUsuario.html')
-
-    except Exception as e:
-        # En caso de error, deshace la transacción
-        conn.rollback()
-        return jsonify({"error": str(e)}), 400
-
-    finally:
-        # Cierra el cursor y la conexión
-        cur.close()
-        db.desconectar(conn)
-    
 
 
 
